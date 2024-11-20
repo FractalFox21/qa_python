3a7a2100c28bc40198f9a7da65e4867d678f4cde
Alex Kravchenko <Morines2320@yandex.ru>
2024-11-20 18:20:38 +0300

    tidy redundant comments

--- tests.py
+++ tests.py
@@ -1,34 +1,21 @@
 import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books_two_books_in_the_dict(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # ------словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2 ---
-        # !!!------ такого словаря и метода в исходном коде нет, поэтому я проверил длину по словарю books_genre, в который добавляются новые книги при работе метода add_new_book
-
-        assert len(collector.books_genre) == 2
-
-
     name_genre = [['Оно', 'Ужасы'],
                   ['Ревизор', 'Комедии'],
                   ]
 
-        # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_two_books_two_books_in_the_dict(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        # ------словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2 ---
+        # !!!------ такого словаря и метода в исходном коде нет, поэтому я проверил длину по словарю books_genre, в который добавляются новые книги при работе метода add_new_book
+        assert len(collector.books_genre) == 2
+
+
     def test_add_new_book_invalid_lenhth_empty_dict(self):      # 1 Проверка на добавление книги с длинным названием
         collector = BooksCollector()
         collector.add_new_book('Что делать, если ваш кот хочет вас убить Что делать, если ваш кот хочет вас убить Что делать, если ваш кот хочет вас убить')  # Превышение длины
@@ -61,16 +48,12 @@ class TestBooksCollector:
         collector = BooksCollector()
         assert collector.get_book_genre("Unknown") is None
 
-
-    def test_get_books_with_specific_requesting_fiction_returns_two_books(self):     # 6 Проверка выборки книг по жанру
+    @pytest.mark.parametrize('name,genre', name_genre)
+    def test_get_books_with_specific_requesting_fiction_returns_two_books(self, name, genre):     # 6 Проверка выборки книг по жанру
         collector = BooksCollector()
-        collector.add_new_book('Гарри Поттер')
-        collector.add_new_book('Властелин колец')
-        collector.add_new_book('Оно')
-        collector.set_book_genre('Гарри Поттер', 'Фантастика')
-        collector.set_book_genre('Властелин колец', 'Фантастика')
-        collector.set_book_genre('Оно', 'Ужасы')
-        assert collector.get_books_with_specific_genre('Фантастика') == ['Гарри Поттер', 'Властелин колец']
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_books_with_specific_genre(genre) == [name]
 
     def test_get_books_with_specific_genre_another_genre_get_empty_list(self):       # 7 Проверка выборки книг по неизвестному жанру
         collector = BooksCollector()
