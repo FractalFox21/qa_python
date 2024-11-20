8352539304f4cd4d865e083fdf9bcce0b8262adc
Alex Kravchenko <Morines2320@yandex.ru>
2024-11-20 17:23:43 +0300

    5 Checking to see if a book that is not in the dictionary has been assigned a genre - test_get_book_genre_unknown_book_entry_is_none
    6 Checking a selection of books by genre - test_get_books_with_specific_requesting_fiction_returns_two_books
    
    7 Checking a selection of books by unknown genre - test_get_books_with_specific_genre_another_genre_get_empty_list
    
    8 Checking for dictionary retrieval books_genre - test_get_book_genre_ask_genre_get_books_genre
    
    9 Checking for a list of children's books - test_get_books_for_children_two_different_books_one_for_children

--- tests.py
+++ tests.py
@@ -23,47 +23,79 @@ class TestBooksCollector:
 
         assert len(collector.books_genre) == 2
 
+
+    name_genre = [['Оно', 'Ужасы'],
+                  ['Ревизор', 'Комедии'],
+                  ]
+
         # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
     def test_add_new_book_invalid_lenhth_empty_dict(self):      # 1 Проверка на добавление книги с длинным названием
         collector = BooksCollector()
         collector.add_new_book('Что делать, если ваш кот хочет вас убить Что делать, если ваш кот хочет вас убить Что делать, если ваш кот хочет вас убить')  # Превышение длины
         assert len(collector.books_genre) == 0
 
-    @pytest.mark.parametrize('name,genre',
-                             [
-                                 ['Оно', 'Ужасы'],
-                                 ['Ревизор', 'Комедии']
-                             ]
-                             )
+    @pytest.mark.parametrize('name,genre', name_genre)
     def test_set_book_genre_set_genre_book_has_a_genre(self, name, genre):      # 2 Проверка на добавление жанра у книги
         collector = BooksCollector()
         collector.add_new_book(name)
         collector.set_book_genre(name, genre)
         assert collector.books_genre[name] == genre
 
-    @pytest.mark.parametrize('name,genre',
-                             [
-                                 ['Оно', 'Ужастик'],
-                                 ['Ревизор', 'Комедия']
-                             ]
-                             )
+    @pytest.mark.parametrize('name,genre', [['Оно', 'Ужастик'],
+                                            ['Ревизор', 'Комедия'],
+                                            ])
     def test_set_book_genre_set_of_unknown_genre_book_has_no_genre(self, name, genre):      # 3 Проверка на попытку ввода неизвестного жанра
         collector = BooksCollector()
         collector.add_new_book(name)
         collector.set_book_genre(name, genre)
         assert collector.books_genre[name] == ''
 
-    @pytest.mark.parametrize('name,genre',
-                             [
-                                 ['Оно', 'Ужасы'],
-                                 ['Ревизор', 'Комедии']
-                             ]
-                             )
-    def test_get_book_genre_ask_genre_get_the_genre(self, name, genre):  # 4 Проверка на получение жанра книги по названию
+    @pytest.mark.parametrize('name,genre', name_genre)
+    def test_get_book_genre_ask_genre_get_book_genre(self, name, genre):  # 4 Проверка на получение жанра книги по названию
         collector = BooksCollector()
         collector.add_new_book(name)
         collector.set_book_genre(name, genre)
         assert collector.get_book_genre(name) == genre
+
+    def test_get_book_genre_unknown_book_entry_is_none (self):      # 5 Проверка получения жанра у книги, которой нет в словаре
+        collector = BooksCollector()
+        assert collector.get_book_genre("Unknown") is None
+
+
+    def test_get_books_with_specific_requesting_fiction_returns_two_books(self):     # 6 Проверка выборки книг по жанру
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер')
+        collector.add_new_book('Властелин колец')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        collector.set_book_genre('Властелин колец', 'Фантастика')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Гарри Поттер', 'Властелин колец']
+
+    def test_get_books_with_specific_genre_another_genre_get_empty_list(self):       # 7 Проверка выборки книг по неизвестному жанру
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        assert collector.get_books_with_specific_genre('Лит РПГ') == []
+
+    @pytest.mark.parametrize('name,genre', name_genre)
+    def test_get_book_genre_ask_genre_get_books_genre(self, name, genre):  # 8 Проверка на получение словаря books_genre
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_books_genre() == {name : genre}
+
+    def test_get_books_for_children_two_different_books_one_for_children(self): # 9 Проверка на получение списка детских книг
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert collector.get_books_for_children() == ['Гарри Поттер']
+
+
+
+
 
 
 
